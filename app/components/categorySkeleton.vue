281fb369fa3ff7d77f2747c3bdc306bc02fa4784
MoMo <8466103+songguo405@user.noreply.gitee.com>
2026-07-28 22:09:25 +0800

    devNav-day3:骨架组件

---
+++ app/components/categorySkeleton.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center gap-4 border border-subtle p-4 rounded-md">
+        <USkeleton class="size-12" />
+        <div>
+            <USkeleton class="h-8 w-full" />
+            <USkeleton class="h-6 w-full" />
+            <div class="flex flex-wrap gap-2 mt-2">
+                <USkeleton class="h-6 w-[80px]" v-for="tag in 4" color="primary" variant="solid" />
+            </div>
+        </div>
+    </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,45 @@
 <template>
   <div>
     <div class="flex items-center gap-2 mb-4">
-      <UButton size="lg" color="neutral" activeColor="primary" variant="outline"
-        activeVariant="solid" :active="currentCategory === -1"
-        class="font-bold rounded-full cursor-pointer" @click="currentCategory = -1;switchCategory()">{{ '全部' }}</UButton>
-      <UButton v-for="item in categorys" size="lg" color="neutral" activeColor="primary" variant="outline"
-        activeVariant="solid" :active="currentCategory === item.id" :key="item.name"
-        class="font-bold rounded-full cursor-pointer" @click="currentCategory = item.id;switchCategory(item.id)">{{ item.name }}</UButton>
+      <UButton
+        size="lg"
+        color="neutral"
+        activeColor="primary"
+        variant="outline"
+        activeVariant="solid"
+        :active="currentCategory === -1"
+        class="font-bold rounded-full cursor-pointer"
+        @click="
+          currentCategory = -1;
+          switchCategory();
+        "
+        >{{ "全部" }}</UButton
+      >
+      <UButton
+        v-for="item in categorys"
+        size="lg"
+        color="neutral"
+        activeColor="primary"
+        variant="outline"
+        activeVariant="solid"
+        :active="currentCategory === item.id"
+        :key="item.name"
+        class="font-bold rounded-full cursor-pointer"
+        @click="
+          currentCategory = item.id;
+          switchCategory(item.id);
+        "
+        >{{ item.name }}</UButton
+      >
     </div>
     <div class="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-4">
-      <CategoryCard v-for="item in navigationList" :key="item.id" :category="item" />
+      <CategorySkeleton v-if="loading" v-for="item in 4" :key="item" />
+      <CategoryCard
+        v-else
+        v-for="item in navigationList"
+        :key="item.id"
+        :category="item"
+      />
     </div>
   </div>
 </template>
@@ -24,7 +54,5 @@ useSeoMeta({
     "A navigation tool for developers to quickly access various resources and tools.",
 });
 
-onMounted(() => {
-
-});
+onMounted(() => {});
 </script>
